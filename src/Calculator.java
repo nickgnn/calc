af3bf0bafcad55ad4d30aee7a0c833ad78c0cda4
nick <gnn@mm.st>
2019-06-20 20:59:16 +0300

    add Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,19 @@
 public class Calculator {
-    private int a;
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
+
+
