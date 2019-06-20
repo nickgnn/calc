33ce17e5a7365de155dbab7a786389e32e5edaee
nick <gnn@mm.st>
2019-06-20 20:59:44 +0300

    add launch

--- src/CalculatorLaunch.java
+++ src/CalculatorLaunch.java
@@ -1,5 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalculatorLaunch {
     public static void main(String[] args) {
+        String input;
 
+        try {
+            while (true) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.print("Input:\n");
+                input = reader.readLine();
+
+                if (input.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                System.out.println("Output:\n" + new StringParcer().parseString(input) + '\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
